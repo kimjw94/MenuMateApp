96fcc5be5afae4cbd6e43c0fff4a3e1e3db3199f
kimjw94 <152935868+kimjw94@users.noreply.github.com>
2024-06-26 10:56:43 +0900

    vue+Spring boot menumateApp 시작

---
+++ src/test/java/com/jw/menumateapp/MenuMateAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jw.menumateapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenuMateAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
