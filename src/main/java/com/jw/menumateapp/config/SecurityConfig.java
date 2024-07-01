4b9336945d0a1d9b7cb46820232ed4bae602418f
kimjw94 <152935868+kimjw94@users.noreply.github.com>
2024-07-01 11:35:38 +0900

    변경된 Home 화면

--- src/main/java/com/jw/menumateapp/config/SecurityConfig.java
+++ src/main/java/com/jw/menumateapp/config/SecurityConfig.java
@@ -1,11 +1,27 @@
 package com.jw.menumateapp.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-	
-	}
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+            .authorizeHttpRequests(authorize -> authorize
+                .antMatchers("/admin").hasRole("ADMIN")
+                .anyRequest().authenticated()
+            )
+            .formLogin(withDefaults()) // 로그인 폼을 사용
+            .logout(withDefaults()); // 로그아웃 설정 추가
+
+        return http.build();
+    }
+}
